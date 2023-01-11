7a580fa9fa9f5a16c5fed3efb8c4096190de721d
Dmitriy <dizel007@yandex.ru>
2023-01-11 15:37:25 +0500

    Норм вывод в таблице(загрузка на сайт)

--- parce_kp/nomenclatura.php
+++ parce_kp/nomenclatura.php
@@ -5,6 +5,10 @@ require_once '../PHPExcel-1.8/Classes/PHPExcel/IOFactory.php';
 require_once '../connect_db.php';
 
 (isset($_GET['words']))?$words=trim($_GET['words']):$words='';
+for ($i22=0; $i22<100; $i22++) {
+$words = str_replace('  ', ' ', $words); // тупо убираем двойные пробелы
+}
+
 (isset($_GET['kolvo_find']))?$kolvo_find=$_GET['kolvo_find']:$kolvo_find='';
 (isset($_GET['FinishContract']))?$FinishContract=1:$FinishContract=0;
 (isset($_GET['dateStart']))?$dateStart=$_GET['dateStart']:$dateStart='';
@@ -17,7 +21,7 @@ echo <<<HTML
 <label for="words">слова для поиска</label>
 <input required type="text" name="words" value="$words">
 
-<label for="kolvo_find">количество</label>
+<label for="kolvo_find"> Кол-во</label>
 <input type="number" name="kolvo_find" value="$kolvo_find">
 
 <label for="FinishContract">Искать в Закрытых КП</label>
@@ -29,10 +33,11 @@ echo <<<HTML
 
 <label for="dateStop">Дата окончания</label>
 <input type="date" name="dateStop">
-<br>
 
-<input type="submit"  value="Send">
+
+<input type="submit"  value="Отправить запрос">
 </form>
+<br>
 HTML;
 ($words=='')?die('Введите слова для поиска'):$words=$words;
 
@@ -43,7 +48,7 @@ $where = "";
 
   if ($FinishContract  == '0') { // если ищем только в открытых КП
   $where = "AND `FinishContract` =:FinishContract";
-  $stmt = $pdo->prepare("SELECT id, KpNumber, KpData, LinkKp,	adress  FROM reestrkp 
+  $stmt = $pdo->prepare("SELECT *  FROM reestrkp 
   WHERE `KpData` >=:dateStart AND `KpData` <=:dateStop $where");
 
    $stmt->execute(array(':dateStart' => $dateStart,
@@ -52,7 +57,7 @@ $where = "";
     ));
 
   } else { // если ищем по всме КП и закрытым тоже
-    $stmt = $pdo->prepare("SELECT id, KpNumber, KpData, LinkKp,	adress  FROM reestrkp 
+    $stmt = $pdo->prepare("SELECT *  FROM reestrkp 
     WHERE `KpData` >=:dateStart AND `KpData` <=:dateStop");
   
      $stmt->execute(array(':dateStart' => $dateStart,
@@ -119,63 +124,43 @@ while ($stop <> 1 ) {
           'KpData' => $value['KpData'],
           'adress' => $value['adress'],
           'id' => $value['id'],
+          'Responsible' => $value['Responsible'],
+          'InnCustomer' => $value['InnCustomer'],
+          'NameCustomer' => $value['NameCustomer'],
+          'type_product' => $value['type_product'],
+          'type_kp' => $value['type_kp'],
+          
+
         );
 
         
     }
-
-
-
-
   }
 
 $i++;
 }
-// if ($priz_name_kp) { // если было вхождение, то выводим эту строку
-
-//   $KpNumber = "<b>КП№".$value['KpNumber']." от ".$value['KpData']." (".$value['adress'].")</b>";  // скачать КП
-//   $link_to_kp_by_id="?transition=10&id=".$value['id']; // выход в реестр к этому КП
-//   $open_kp_by_id="open_excel/parce_excel_kp.php?LinkKp=".$LinkKp; // посмотреть КП
-
-//   echo <<<HTML
-  
-//     <a href="../$open_kp_by_id" target="_blank"><b>$KpNumber</b></a>
-//     <a href="../$link_to_kp_by_id"><b>**Перейти к КП*</b></a>
-//     <a href="../$LinkKp"><b>*Скачать КП**</b></a>
-
-
-// HTML;
-// // Рисуем таблицу  УП
-// echo "<table>";
-// foreach ($prods as $value1) {
-//     foreach ($value1 as $item) {
-//       $name_temp= $item["name"];
-//       $ed_izm_temp= $item["ed_izm"];
-//       $kolvo_temp= $item["kol"];
-
-// echo <<<HTML
-    
-//     <tr>
-//         <td>$name_temp<td>
-//         <td>$ed_izm_temp<td>
-//         <td>$kolvo_temp<td>
-
-//     </tr>
-    
-// HTML;
-
-//     }
-//   }
-//   echo "</table><br>";
-// }
   unset($prods);
-
 } 
 };
 
 
- 
+// if ((isset($prods_all)) OR ((count(@$prods_all)) == 0)) {
+if ((!isset($prods_all))) {
+  die('Нет данных для вывода');
+}
+echo "Количество найденных КП :".count(@$prods_all);
 
+$prods_all = mod_words_strong($words, $prods_all); // Подчеркиваем найденные слова
+
+
+//  echo "<pre>";
+//  print_r($prods_all);
+//  echo "<pre>";
+
+$smarty->assign('prods_all', $prods_all);
+$smarty->assign('pageName', 'Поиск продукции по номенклатуре');
+
+$smarty ->display('../templates/find_nomeclaturu.tpl');
 
 
 
@@ -184,6 +169,7 @@ function mod_words($words) { // функция приводит строку к 
   $temp_words = str_replace(' ', '', $temp_words);
   $temp_words = str_replace('–', '-', $temp_words);
   $temp_words = str_replace(array("\n","\r"), '', $temp_words);
+ 
   return $temp_words;
 }
 
@@ -191,7 +177,16 @@ function mod_words($words) { // функция приводит строку к 
 function find_words_in_nomenclature($words, $name, $kolvo_find, $kolvo) { // функция ищет вхождение всех слов поиска в строке наименования
 $result = false;
 $count_inputs=0; // сколько уникальных вхождений
-$find_arr = explode(' ', $words); 
+$find_arr = explode(' ', $words);
+// $count_find_arr = count($find_arr) - 1;
+// for ($i=0; $count_find_arr; $i++) {
+//   if ($find_arr[$i] == '') {
+//     unset($find_arr[$i]);
+//     $count_find_arr--;
+//   }
+// }
+
+
 $arr_count = count($find_arr);
 foreach ($find_arr as $item) {
   if (strpos(mod_words($name),mod_words($item))) {
@@ -209,12 +204,25 @@ if (($kolvo_find <> 0) AND ($kolvo_find != $kolvo)) {
 }
 
 
-echo "Количество найденных КП :".count(@$prods_all);
 
-//   echo "<pre>";
-//  print_r($prods_all);
-//  echo "<pre>";
-$smarty->assign('prods_all', $prods_all);
-$smarty->assign('pageName', 'Поиск продукции по номенклатуре');
+function mod_words_strong($words, &$prods_all) { // функция выделяет слова жирным приводит строку к стандартному виду
+$words = mb_strtolower($words);
+$find_arr_words = explode(' ', $words); 
 
-$smarty ->display('../templates/find_nomeclaturu.tpl');
+  foreach ($prods_all as &$items) {
+    foreach ($items as &$one_item) {
+      $one_item['name'] = substr($one_item['name'], 1);
+      
+     
+      foreach ($find_arr_words as $f_word) {
+    
+        $one_item['name'] = str_replace($f_word, "<b><u>".$f_word."</u></b>", mb_strtolower($one_item['name']));
+       
+      }
+    }
+  }
+
+  return $prods_all;
+}
+
+
